fad5d80f08aab9ba250c3d58b8b9e4e89b8f9e1c
Simon Fan <simon@orioro.design>
2021-01-18 21:59:13 -0300

    Changes validation error text format

--- src/util/validate.ts
+++ src/util/validate.ts
@@ -11,23 +11,29 @@ export const isValidNumber = value => {
   return typeof value === 'number' && value !== NaN
 }
 
+const mightBeExpression = (value:any) => (
+  Array.isArray(value) &&
+  typeof value[0] === 'string' &&
+  value[0].startsWith('$')
+)
+
 const validateType = (typeName, test) => {
   const validate = value => {
     if (!test(value)) {
-      throw new TypeError(`Invalid '${typeName}': ${JSON.stringify(value)}`)
+      throw new TypeError(`Invalid ${typeName}: ${JSON.stringify(value)}`)
     }
   }
 
   validate.allowUndefined = value => {
     if (value !== undefined && !test(value)) {
-      throw new TypeError(`Invalid '${typeName}': ${JSON.stringify(value)}`)
+      throw new TypeError(`Invalid ${typeName}: ${JSON.stringify(value)}`)
     }
   }
 
   return validate
 }
 
-export const validateNumber = validateType('valid number', isValidNumber)
+export const validateNumber = validateType('valid_number', isValidNumber)
 export const validateArray = validateType('array', isArray)
 export const validateString = validateType('string', isString)
 export const validatePlainObject = validateType('plain object', isPlainObject)
